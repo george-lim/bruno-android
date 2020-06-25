1df5b81fc2eaa2650d2d2531609d0ae68320e23c
George Lim <lim.george@pm.me>
2020-06-25 02:02:46 -0400

    Add typo to MapsActivity.java to cause build error

--- app/src/main/java/com/cs446/group7/bruno/MapsActivity.java
+++ app/src/main/java/com/cs446/group7/bruno/MapsActivity.java
@@ -19,7 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivit extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
