10243f8a0ceec1ad12a86a9ac3aae73ad01e61e1
Jackie Xu <jackiexu2007@hotmail.com>
2020-06-25 00:36:03 -0400

    Add basic foundation (#1)
    * Basic foundation
    
    * Update target SDK version to 30
    
    * Invalidate and remove test API key
    
    * Update app/src/main/AndroidManifest.xml
    
    Great catch
    
    Co-authored-by: Boris Chan <boris1022chan@users.noreply.github.com>
    
    * Add API 30 documentation to README.md
    
    * Clean up redundency in .gitignore
    
    * Add more .gitignore entries
    
    * Add API key setup
    
    * Add google_maps_api.xml to gitignore
    
    * Update INSTALL.sh with API key injection
    
    * Update untracked files
    
    * Update INSTALL.sh to work from any directory, and fix file not found for API key
    
    * Remove duplicate code in INSTALL.sh
    
    * Fix INSTALL script bug regarding mkdir
    
    * Remove duplicate code
    - Add lint supression to remove the permission warning
    - Re-arrange if-statement to avoid negative conditions
    
    Co-authored-by: George Lim <lim.george@pm.me>
    Co-authored-by: Boris Chan <boris1022chan@users.noreply.github.com>
    Co-authored-by: George Lim <21700768+george-lim@users.noreply.github.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# cd to project root
+cd `dirname $0`
+
 # Copy hooks
 cp hooks/* .git/hooks
+
+# Install API key (if found)
+google_maps_key=${1:-'<!-- Add API Key here -->'}
+google_maps_api_xml="<resources>\n\t<string name=\"google_maps_key\" templateMergeStrategy=\"preserve\" translatable=\"false\">\n\t\t$google_maps_key\n\t</string>\n</resources>"
+
+for config in debug release; do
+  api_key_dir=app/src/$config/res/values
+  mkdir -p $api_key_dir
+  echo -e $google_maps_api_xml > $api_key_dir/google_maps_api.xml
+done
