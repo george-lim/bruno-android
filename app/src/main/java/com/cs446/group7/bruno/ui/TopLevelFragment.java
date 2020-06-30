bba3aa943c5e79bd89f8df3fb050dfb8c07736f3
Boris Chan <boris1022chan@gmail.com>
2020-06-30 13:06:31 -0400

    clean up files

--- app/src/main/java/com/cs446/group7/bruno/ui/TopLevelFragment.java
+++ app/src/main/java/com/cs446/group7/bruno/ui/TopLevelFragment.java
@@ -1,4 +1,4 @@
-package com.cs446.group7.bruno;
+package com.cs446.group7.bruno.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.cs446.group7.bruno.R;
 import com.cs446.group7.bruno.ui.BottomNavPagerAdaptor;
 import com.cs446.group7.bruno.ui.fitnessrecords.FitnessRecordsFragment;
 import com.cs446.group7.bruno.ui.routeplanning.RoutePlanningFragment;
