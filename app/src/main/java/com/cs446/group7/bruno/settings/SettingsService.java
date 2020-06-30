754ebb7e019147f02102c56bfd411b6a439c6a89
Sherry Li <sherryhli@users.noreply.github.com>
2020-06-30 12:17:25 -0400

    Modify precision of PREFERRED_WALKING_SPEED

--- app/src/main/java/com/cs446/group7/bruno/settings/SettingsService.java
+++ app/src/main/java/com/cs446/group7/bruno/settings/SettingsService.java
@@ -4,5 +4,5 @@ package com.cs446.group7.bruno.settings;
 public class SettingsService {
     public static final int DEFAULT_EXERCISE_DURATION = 60;
     // metres per minute, based on Google Map's walking speed of 5 km /hr
-    public static final double PREFERRED_WALKING_SPEED = 83.333;
+    public static final double PREFERRED_WALKING_SPEED = 5000d / 60d;
 }
