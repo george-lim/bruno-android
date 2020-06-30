284b4044fe35f345e75d90d8be67845f679b17ad
Boris Chan <boris1022chan@gmail.com>
2020-06-30 11:59:54 -0400

    move viewpapger and bottom navigation to topLvlFragment

--- app/src/main/java/com/cs446/group7/bruno/MainActivity.java
+++ app/src/main/java/com/cs446/group7/bruno/MainActivity.java
@@ -1,88 +1,34 @@
 package com.cs446.group7.bruno;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.cs446.group7.bruno.ui.BaseFragment;
-import com.cs446.group7.bruno.ui.BottomNavPagerAdaptor;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    private ViewPager2 viewPager;
-    private BottomNavigationView bttmNav;
-    private List<BaseFragment> baseFragments = new ArrayList<>();
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupBottomNavigation();
+
     }
 
-    private void setupBottomNavigation() {
-        viewPager = findViewById(R.id.main_screen_pager);
-        bttmNav = findViewById(R.id.bttm_nav);
-        BottomNavPagerAdaptor adaptor = new BottomNavPagerAdaptor(this);
-        baseFragments.add(BaseFragment.newInstance(R.layout.content_route_base, R.id.nav_host_route));
-        baseFragments.add(BaseFragment.newInstance(R.layout.content_fitness_base, R.id.nav_host_fitness));
-        baseFragments.add(BaseFragment.newInstance(R.layout.content_setting_base, R.id.nav_host_settings));
-        for (Fragment f: baseFragments) {
-            adaptor.addFragment(f);
-        }
-        viewPager.setAdapter(adaptor);
-        viewPager.setUserInputEnabled(false);
-        viewPager.setOffscreenPageLimit(3);
-        bttmNav.setOnNavigationItemSelectedListener(this);
-        viewPager.registerOnPageChangeCallback(pageChangeCallback);
-    }
-
-    ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
-        @Override
-        public void onPageSelected(int position) {
-            switch (position) {
-                case 0:
-                    bttmNav.setSelectedItemId(R.id.navigate_route_planning); break;
-                case 1:
-                    bttmNav.setSelectedItemId(R.id.navigate_fitness_records); break;
-                case 3:
-                    bttmNav.setSelectedItemId(R.id.navigate_settings); break;
-            }
-            super.onPageSelected(position);
-        }
-    };
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.navigate_route_planning:
-                viewPager.setCurrentItem(0, false); break;
-            case R.id.navigate_fitness_records:
-                viewPager.setCurrentItem(1, false); break;
-            case R.id.navigate_settings:
-                viewPager.setCurrentItem(2, false); break;
-        }
-        return true;
-    }
-
+//    TODO: fix this
     @Override
     public void onBackPressed() {
-        BaseFragment curFragment = baseFragments.get(viewPager.getCurrentItem());
-        boolean canNavigateUp = curFragment.onBackPressed();
-        if (canNavigateUp) {
-            return;
-        } else if (viewPager.getCurrentItem() != 0) {
-            viewPager.setCurrentItem(0, false);
-        } else {
-            super.onBackPressed();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        int id = navController.getCurrentDestination().getId();
+        if (id == R.id.fragment_top_lvl) {
+            ViewPager2 pager = findViewById(R.id.main_screen_pager);
+            int pos = pager.getCurrentItem();
+            if (pos != 0) {
+                pager.setCurrentItem(0, false);
+                return;
+            }
         }
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/cs446/group7/bruno/TopLevelFragment.java
@@ -0,0 +1,78 @@
+package com.cs446.group7.bruno;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.cs446.group7.bruno.ui.BottomNavPagerAdaptor;
+import com.cs446.group7.bruno.ui.fitnessrecords.FitnessRecordsFragment;
+import com.cs446.group7.bruno.ui.routeplanning.RoutePlanningFragment;
+import com.cs446.group7.bruno.ui.settings.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class TopLevelFragment extends Fragment implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private ViewPager2 viewPager;
+    private BottomNavigationView bttmNav;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_top_level, container, false);
+        setupBottomNavigation(root);
+        return root;
+    }
+
+    private void setupBottomNavigation(View view) {
+        viewPager = view.findViewById(R.id.main_screen_pager);
+        bttmNav = view.findViewById(R.id.bttm_nav);
+        BottomNavPagerAdaptor adaptor = new BottomNavPagerAdaptor(getActivity());
+        adaptor.addFragment(new RoutePlanningFragment());
+        adaptor.addFragment(new FitnessRecordsFragment());
+        adaptor.addFragment(new SettingsFragment());
+        viewPager.setAdapter(adaptor);
+        viewPager.setUserInputEnabled(false);
+        viewPager.setOffscreenPageLimit(3);
+        bttmNav.setOnNavigationItemSelectedListener(this);
+        viewPager.registerOnPageChangeCallback(pageChangeCallback);
+    }
+
+    ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
+        @Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                    bttmNav.setSelectedItemId(R.id.navigate_route_planning);
+                    break;
+                case 1:
+                    bttmNav.setSelectedItemId(R.id.navigate_fitness_records);
+                    break;
+                case 3:
+                    bttmNav.setSelectedItemId(R.id.navigate_settings);
+                    break;
+            }
+            super.onPageSelected(position);
+        }
+    };
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigate_route_planning:
+                viewPager.setCurrentItem(0, false);
+                break;
+            case R.id.navigate_fitness_records:
+                viewPager.setCurrentItem(1, false);
+                break;
+            case R.id.navigate_settings:
+                viewPager.setCurrentItem(2, false);
+                break;
+        }
+        return true;
+    }
+}
