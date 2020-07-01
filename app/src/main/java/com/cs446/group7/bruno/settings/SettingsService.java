fd23f0f9b5b09a2d450282d14dd40d36509b8937
Jackie Xu <jackiexu2007@hotmail.com>
2020-07-01 16:06:54 -0400

    Route generation logic (#4)
    * Route generation concept
    
    * Routing cleanup
    
    * Add RouteGenerator, test UI and mock paths
    
    * Add distance conversion to lat-lng degree logic
    
    * Separate mock and real implementation of RouteGenerator using bridge pattern
    
    * Making mock contain original information
    
    * Change parsing responsibility
    
    * Add UI for inputting exercise duration
    
    * Set number of points to always be 3
    
    * Modify precision of PREFERRED_WALKING_SPEED
    
    * Add ACCESS_NETWORK_STATE permission to AndroidManifest.xml
    
    Co-authored-by: Boris Chan <boris1022chan@users.noreply.github.com>
    
    * Remove unused code
    
    * Refactor RouteGenerator and implementations
    
    * Add onRouteError callback
    
    * Refactor Route to include RouteSegment, move parsing to RouteGenerator
    
    * Add OTHER_ERROR to MapsActivity's onRouteError switch statement
    
    Co-authored-by: George Lim <21700768+george-lim@users.noreply.github.com>
    
    * Include underlyingException as parameter to onRouteError
    
    Co-authored-by: Sherry Li <sherryhli@users.noreply.github.com>
    Co-authored-by: Boris Chan <boris1022chan@users.noreply.github.com>
    Co-authored-by: George Lim <21700768+george-lim@users.noreply.github.com>

---
+++ app/src/main/java/com/cs446/group7/bruno/settings/SettingsService.java
@@ -0,0 +1,8 @@
+package com.cs446.group7.bruno.settings;
+
+// nothing much here, just some hardcoded values for now
+public class SettingsService {
+    public static final int DEFAULT_EXERCISE_DURATION = 60;
+    // metres per minute, based on Google Map's walking speed of 5 km /hr
+    public static final double PREFERRED_WALKING_SPEED = 5000d / 60d;
+}
