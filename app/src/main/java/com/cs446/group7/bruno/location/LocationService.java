cec8c5a53680411e7deb26d0b02d08af9a6deb3c
Jackie Xu <jackiexu2007@hotmail.com>
2020-07-04 23:12:13 -0400

    Add location update logic

--- app/src/main/java/com/cs446/group7/bruno/location/LocationService.java
+++ app/src/main/java/com/cs446/group7/bruno/location/LocationService.java
@@ -3,25 +3,68 @@ package com.cs446.group7.bruno.location;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Location;
+import android.os.Looper;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
+/**
+ * Singleton responsible for handling all logic related to querying the location
+ */
+public class LocationService extends LocationCallback {
 
-public class LocationService {
+    private static final int UPDATE_INTERVAL_MILLISECONDS = 5000;
+    private static final int FASTEST_UPDATE_INTERVAL_MILLISECONDS = 2000;
+
     private FusedLocationProviderClient fusedLocationClient;
     private List<LocationServiceSubscriber> subscriberList;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private static String TAG;
 
-    public LocationService(final Context context) {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    private static LocationService instance;
+
+    public static void init(Context context) {
+        if (instance != null) {
+            Log.w(TAG, "init called again, singleton re-initialized");
+        }
+        instance = new LocationService(context);
+    }
+
+    public static LocationService getInstance() {
+        if (instance == null) {
+            throw new AssertionError("you must call 'init' first!");
+        }
+        return instance;
+    }
+
+    private LocationService(final Context context) {
         subscriberList = new ArrayList<>();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+        locationRequest = LocationRequest.create();
+
+        // Desired duration at which updates are received
+        locationRequest.setInterval(UPDATE_INTERVAL_MILLISECONDS);
+
+        // Fastest interval that we can handle requests. eg. It can handle updates as fast as every 2000 ms
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_MILLISECONDS);
+
+        // Prioritize high accuracy results
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        TAG = getClass().getSimpleName();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, this, Looper.getMainLooper());
     }
 
     public void addSubscriber(final LocationServiceSubscriber subscriber) {
@@ -58,5 +101,20 @@ public class LocationService {
                 .addOnFailureListener(error -> {
                     callback.onLocationUpdateFailure(new LocationServiceException(error));
                 });
+    }
+
+    @Override
+    public void onLocationResult(LocationResult locationResult) {
+        final Location result = locationResult.getLastLocation();
+        if (result == null) {
+            for (LocationServiceSubscriber subscriber : subscriberList) {
+                subscriber.onLocationUpdateFailure(
+                        new LocationServiceException(LocationServiceException.ErrorType.NULL_LOCATION_ERROR));
+            }
+        } else {
+            for (LocationServiceSubscriber subscriber : subscriberList) {
+                subscriber.onLocationUpdateSuccess(result);
+            }
+        }
     }
 }
