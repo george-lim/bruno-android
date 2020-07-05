99f31f9b6a59ccf1f31275a7da732cdd67114d0e
Mohit Patel <m265pate@uwaterloo.ca>
2020-07-05 17:49:44 -0400

    Modified install script to download Spotify aar file

--- INSTALL.sh
+++ INSTALL.sh
@@ -30,6 +30,14 @@ for config in debug release; do
   echo -e $spotify_client_id_xml > $api_key_dir/spotify_client_id.xml
   echo -e $spotify_client_secret_xml > $api_key_dir/spotify_client_secret.xml
   echo -e $spotify_redirect_uri_xml > $api_key_dir/spotify_redirect_uri.xml
-
-
 done
+
+# Install spotify-app-remote library if it does not exist
+spotify_app_remote_libary_location=https://github.com/spotify/android-sdk/releases/download/v7.0.0-appremote_v1.2.3-auth/spotify-app-remote-release-0.7.0.aar 
+
+spotify_app_remote_release_dir=dependencies/spotify-app-remote
+spotify_app_remote_release_file=spotify-app-remote-release-0.7.0.aar
+if [ ! -f "$spotify_app_remote_release_dir/$spotify_app_remote_release_file" ]; then
+  echo "Spotify app remote library missing, downloading from Github"
+  curl -L "$spotify_app_remote_libary_location" --output "$spotify_app_remote_release_dir/$spotify_app_remote_release_file"
+fi
