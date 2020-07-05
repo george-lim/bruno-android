8a814c590594bbdfb93ebdac615d677d266d292b
Boris Chan <boris1022chan@users.noreply.github.com>
2020-07-04 20:05:56 -0400

    App navigation structure (#6)
    * add color scheme
    
    * create mainActivity, fragments, and navgraph
    
    * add bottom navigation to mainAcitvity
    
    * fix navigation up behaviour when layer from base fragment
    
    * move viewpapger and bottom navigation to topLvlFragment
    
    * modify trigger to fitnessDetailsFragment
    
    * clean up files
    
    * add appbar behaviour
    
    * add navigation to onRouteFragment
    
    * clean up unused layout xml
    
    * add code documentation on navigation hack
    
    * encapsulate viewpager select logic in TopLevelFragment
    
    * remove onActivityCreated since it is deprecated
    
    * rename to be consistent with naming convention of fragment id
    
    * remove FragmentToolbar builder pattern to a method call
    
    * nuke MapsActivity
    
    * simiplify color.xml
    
    * remove instantiation of FragmentToolbarFormatter

---
+++ app/src/main/java/com/cs446/group7/bruno/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cs446.group7.bruno;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.cs446.group7.bruno.ui.toplevel.TopLevelFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * We want the app to exit only if the app is currently on the map tab.
+     * The setup of nav_graph will exit the app if it display TopLevelFragment.
+     * Hence, overriding the behaviour when pressing the back button will achieve
+     * the desired result.
+     */
+    @Override
+    public void onBackPressed() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        int curContainerFragmentId = navController.getCurrentDestination().getId();
+        if (curContainerFragmentId == R.id.fragment_top_lvl) {
+            NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().getPrimaryNavigationFragment();
+            TopLevelFragment topLvlFragment = (TopLevelFragment) navHostFragment.getChildFragmentManager().getPrimaryNavigationFragment();
+            if (topLvlFragment.onBackPress()) return;
+        }
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/cs446/group7/bruno/ui/fitnessrecords/FitnessRecordsFragment.java
@@ -0,0 +1,39 @@
+package com.cs446.group7.bruno.ui.fitnessrecords;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.cs446.group7.bruno.R;
+import com.cs446.group7.bruno.ui.AppbarFormatter;
+
+public class FitnessRecordsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_fitness_records, container, false);
+        AppbarFormatter.format(
+                (AppCompatActivity) getActivity(),
+                view,
+                R.id.appbar_fitness_records,
+                getResources().getString(R.string.title_fitness_records),
+                false);
+
+        Button toDetailsBtn = view.findViewById(R.id.button_to_details);
+        toDetailsBtn.setOnClickListener(this::handleNavigateToFitnessDetailsClick);
+        return view;
+    }
+
+    private void handleNavigateToFitnessDetailsClick(final View view) {
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.action_fragmenttoplevel_to_fragmentfitnessdetails);
+    }
+}
