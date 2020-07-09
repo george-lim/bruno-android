29770503419976dfdbe99ab0ea6897b259dacc12
patelmohit <35036073+patelmohit@users.noreply.github.com>
2020-07-08 20:21:27 -0400

    Mock Spotify integration, interfaces (#17)
    * Added interfaces and mock classes for player and playlist
    
    * Added extra logging
    
    * Added TAG variable for logging, different artist name for getBrunoTrack
    
    * Changed snake case to camelCase and added generic list

---
+++ app/src/main/java/com/cs446/group7/bruno/music/BrunoPlaylist.java
@@ -0,0 +1,24 @@
+package com.cs446.group7.bruno.music;
+
+import java.util.List;
+
+// Class which encapsulates all playlist information used by our app
+// Contains track information through a list of BrunoTracks
+public class BrunoPlaylist {
+
+    public final String name;
+    public final String description;
+    public final int totalTracks;
+    public final long totalDuration;
+    public final List<BrunoTrack> tracks;
+
+    public BrunoPlaylist(final String name, final String description, final int totalTracks,
+                         final long totalDuration, final List<BrunoTrack> tracks) {
+        this.name = name;
+        this.description = description;
+        this.totalTracks = totalTracks;
+        this.totalDuration = totalDuration;
+        this.tracks = tracks;
+    }
+
+}
---
+++ app/src/main/java/com/cs446/group7/bruno/music/BrunoTrack.java
@@ -0,0 +1,21 @@
+package com.cs446.group7.bruno.music;
+
+import java.util.List;
+
+// Simple container class containing information about a track
+public class BrunoTrack {
+
+    public final String name;
+    public final String album;
+    public final long duration; // Milliseconds
+    public final List<String> artists;
+
+    public BrunoTrack(final String name, final String album, final long duration,
+                      final List<String> artists) {
+        this.name = name;
+        this.album = album;
+        this.duration = duration;
+        this.artists = artists;
+    }
+
+}
