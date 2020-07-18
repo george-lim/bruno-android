62ff6b0df4a8fe990386a57987bf60b6629e085f
Sherry Li <sherryhli@users.noreply.github.com>
2020-07-18 00:35:10 -0400

    New RouteSegment calculation (#30)
    * Calculate RouteSegments from decodedPath
    
    * Add final to parameters
    
    * Remove RouteSegment test code from RoutePlanningFragment
    
    * Revise distance calculation to use haversine formula
    
    * Use walking speed constant from SettingsService
    
    * Move LatLngUtils to utils folder

--- app/src/main/java/com/cs446/group7/bruno/routing/Route.java
+++ app/src/main/java/com/cs446/group7/bruno/routing/Route.java
@@ -1,20 +1,39 @@
 package com.cs446.group7.bruno.routing;
 
+import com.cs446.group7.bruno.settings.SettingsService;
+import com.cs446.group7.bruno.utils.LatLngUtils;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.PolyUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Route {
     private List<LatLng> decodedPath;
     private List<RouteSegment> routeSegments;
 
-    public Route(final String encodedPath, final List<RouteSegment> routeSegments) {
+    public Route(final String encodedPath) {
         this.decodedPath = PolyUtil.decode(encodedPath);
-        this.routeSegments = routeSegments;
+        this.routeSegments = calculateSegments(decodedPath);
     }
 
     public List<LatLng> getDecodedPath() {
         return decodedPath;
+    }
+
+    public List<RouteSegment> getRouteSegments() {
+        return routeSegments;
+    }
+
+    private static List<RouteSegment> calculateSegments(final List<LatLng> decodedPath) {
+        List<RouteSegment> routeSegments = new ArrayList<>();
+        for (int i = 0; i < decodedPath.size() - 1; ++i) {
+            double distanceMetres = LatLngUtils.getLatLngDistanceInMetres(decodedPath.get(i), decodedPath.get(i + 1));
+            // should not overflow since (distanceMetres / WALKING_SPEED) is small enough
+            double durationMs = (distanceMetres / (SettingsService.PREFERRED_WALKING_SPEED / 60)) * 1000;
+            RouteSegment routeSegment = new RouteSegment(decodedPath.get(i), decodedPath.get(i + 1), (long) durationMs);
+            routeSegments.add(routeSegment);
+        }
+        return routeSegments;
     }
 }
