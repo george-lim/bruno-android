b3a2469b9c85352cb502af927fa64a0187572071
Sherry Li <sherryhli@users.noreply.github.com>
2020-07-17 21:04:29 -0400

    Revise distance calculation to use haversine formula

---
+++ app/src/main/java/com/cs446/group7/bruno/routing/LatLngUtils.java
@@ -0,0 +1,20 @@
+package com.cs446.group7.bruno.routing;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LatLngUtils {
+    private static final int EARTH_RADIUS_METRES = 6371000;
+
+    private static double haversine(double theta) {
+        return Math.sin(theta / 2) * Math.sin(theta / 2);
+    }
+
+    public static double getLatLngDistanceInMetres(final LatLng start, final LatLng end) {
+        double diffLatRad = Math.toRadians(end.latitude - start.latitude);
+        double diffLngRad = Math.toRadians(end.longitude - start.longitude);
+
+        // the haversine formula for distance between two points on a sphere
+        return 2 * EARTH_RADIUS_METRES * Math.asin(Math.sqrt(haversine(diffLatRad) +
+                (Math.cos(start.latitude) * Math.cos(end.latitude) * haversine(diffLngRad))));
+    }
+}
--- app/src/main/java/com/cs446/group7/bruno/routing/Route.java
+++ app/src/main/java/com/cs446/group7/bruno/routing/Route.java
@@ -28,24 +28,12 @@ public class Route {
     private static List<RouteSegment> calculateSegments(final List<LatLng> decodedPath) {
         List<RouteSegment> routeSegments = new ArrayList<>();
         for (int i = 0; i < decodedPath.size() - 1; ++i) {
-            // this distance is not accurate, see below. We currently always overestimate distance
-            // so calculated duration is always greater than Google's estimate
-            double distanceMetres = getLatLngDistance(decodedPath.get(i), decodedPath.get(i + 1))
-                    * RouteGenerator.METRES_PER_LAT_DEG;
+            double distanceMetres = LatLngUtils.getLatLngDistanceInMetres(decodedPath.get(i), decodedPath.get(i + 1));
             // should not overflow since (distanceMetres / WALKING_SPEED) is small enough
             double durationMs = (distanceMetres / WALKING_SPEED) * 1000;
             RouteSegment routeSegment = new RouteSegment(decodedPath.get(i), decodedPath.get(i + 1), (long) durationMs);
             routeSegments.add(routeSegment);
         }
         return routeSegments;
-    }
-
-    // Note: not a very accurate calculation since 1 lat deg != 1 lng degree, but the difference
-    // should not be pronounced for short distances. We may need to adjust depending on how
-    // challenging it is for the user to keep the pace that we set
-    private static double getLatLngDistance(final LatLng start, final LatLng end) {
-        double diffLat = end.latitude - start.latitude;
-        double diffLng = end.longitude - start.longitude;
-        return Math.sqrt((diffLat * diffLat) + (diffLng * diffLng));
     }
 }
