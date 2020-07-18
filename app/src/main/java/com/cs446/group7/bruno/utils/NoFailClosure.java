3117b801dc501e8e17b60e743d67560484a48c06
George Lim <21700768+george-lim@users.noreply.github.com>
2020-07-18 12:45:02 -0400

    Add support for previous results in ClosureQueue (#31)
    * Add support for previous results in ClosureQueue
    
    * Add comments for ClosureQueue overload
    
    * Simplify one callback in MainActivity
    
    * Remove unused import from RouteGenerator

--- app/src/main/java/com/cs446/group7/bruno/utils/NoFailClosure.java
+++ app/src/main/java/com/cs446/group7/bruno/utils/NoFailClosure.java
@@ -2,5 +2,5 @@ package com.cs446.group7.bruno.utils;
 
 // Represents an asynchronous piece of code that will eventually call a callback
 public interface NoFailClosure<Success> {
-    void run(NoFailCallback<Success> callback);
+    void run(Success result, NoFailCallback<Success> callback);
 }
