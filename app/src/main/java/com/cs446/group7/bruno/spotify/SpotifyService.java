f113e06532857c2b11edf72def49c0311de209a9
Boris Chan <boris1022chan@gmail.com>
2020-08-02 02:11:54 -0400

    add premium status check

--- app/src/main/java/com/cs446/group7/bruno/spotify/SpotifyService.java
+++ app/src/main/java/com/cs446/group7/bruno/spotify/SpotifyService.java
@@ -2,6 +2,7 @@ package com.cs446.group7.bruno.spotify;
 
 import android.content.Context;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.cs446.group7.bruno.BuildConfig;
 import com.cs446.group7.bruno.spotify.auth.MockSpotifyAuthServiceImpl;
 import com.cs446.group7.bruno.spotify.auth.SpotifyAuthService;
@@ -17,13 +18,25 @@ public class SpotifyService {
     private SpotifyPlayerService playerService;
     private SpotifyPlaylistService playlistService;
 
+    // Default is 2500 MS
+    private static final int REQUEST_TIMEOUT_MS = DefaultRetryPolicy.DEFAULT_TIMEOUT_MS;
+    // Default is 1 retry, but we use 5 instead
+    private static final int REQUEST_MAX_RETRIES = 5;
+    // Default is 1f (i.e. first request waits 2500MS, the next request waits 5000MS, etc...)
+    private static final float REQUEST_BACKOFF_MULT = DefaultRetryPolicy.DEFAULT_BACKOFF_MULT;
+    private static final DefaultRetryPolicy retryPolicy = new DefaultRetryPolicy(
+            REQUEST_TIMEOUT_MS,
+            REQUEST_MAX_RETRIES,
+            REQUEST_BACKOFF_MULT
+    );
+
 
     public SpotifyService(Context context, SpotifyRequestDelegate delegate) {
         authService = BuildConfig.DEBUG
                 ? new MockSpotifyAuthServiceImpl()
-                : new SpotifyAuthServiceImpl(context, delegate);
+                : new SpotifyAuthServiceImpl(context, delegate, retryPolicy);
         playerService = new SpotifyPlayerService();
-        playlistService = new SpotifyPlaylistService(context);
+        playlistService = new SpotifyPlaylistService(context, retryPolicy);
     }
 
     public SpotifyAuthService getAuthService() {
--- app/src/main/java/com/cs446/group7/bruno/spotify/auth/MockSpotifyAuthServiceImpl.java
+++ app/src/main/java/com/cs446/group7/bruno/spotify/auth/MockSpotifyAuthServiceImpl.java
@@ -3,7 +3,13 @@ package com.cs446.group7.bruno.spotify.auth;
 import com.cs446.group7.bruno.utils.Callback;
 
 public class MockSpotifyAuthServiceImpl implements SpotifyAuthService {
+    @Override
     public void requestUserAuth(final Callback<String, Void> clientCallback) {
         clientCallback.onSuccess("token");
+    }
+
+    @Override
+    public void checkIfUserIsPremium(String token, Callback<Boolean, Void> callback) {
+        callback.onSuccess(true);
     }
 }
