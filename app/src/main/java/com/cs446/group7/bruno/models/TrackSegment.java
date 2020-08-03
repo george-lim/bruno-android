3571de14888cb23d2b47a09601d467932ffe0c90
George Lim <21700768+george-lim@users.noreply.github.com>
2020-08-03 17:19:28 -0400

    Redo coordinate calculation logic for Bruno Avatar (#73)
    * Redo coordinate calculation logic for Bruno
    
    * Remove comment
    
    * Add helpful comments
    
    * Rename midPoint to splitPoint

--- app/src/main/java/com/cs446/group7/bruno/models/TrackSegment.java
+++ app/src/main/java/com/cs446/group7/bruno/models/TrackSegment.java
@@ -12,29 +12,30 @@ public class TrackSegment {
     // MARK: - Private members
 
     private List<Coordinate> coordinates;
+    private List<Long> durations;
     private int routeColour;
 
     // MARK: - Lifecycle methods
 
     public TrackSegment(final List<RouteSegment> routeSegments,
                         int routeColour) {
-        coordinates = processCoordinates(routeSegments);
+        processCoordinates(routeSegments);
         this.routeColour = routeColour;
     }
 
     // MARK: - Private methods
 
-    public List<Coordinate> processCoordinates(final List<RouteSegment> routeSegments) {
-        List<Coordinate> coordinates = new ArrayList<>(routeSegments.size());
+    private void processCoordinates(final List<RouteSegment> routeSegments) {
+        coordinates = new ArrayList<>(routeSegments.size());
+        durations = new ArrayList<>(routeSegments.size());
 
         for (RouteSegment routeSegment : routeSegments) {
             coordinates.add(routeSegment.getStartCoordinate());
+            durations.add(routeSegment.getDuration());
         }
 
         RouteSegment lastRouteSegment = routeSegments.get(routeSegments.size() - 1);
         coordinates.add(lastRouteSegment.getEndCoordinate());
-
-        return coordinates;
     }
 
     // MARK: - Public methods
@@ -67,5 +68,28 @@ public class TrackSegment {
         }
 
         return distance;
+    }
+
+    // Returns the coordinate at the playbackPosition of a track
+    public Coordinate getCoordinate(long playbackPosition) {
+        int routeSegmentIndex;
+        long routeSegmentPlaybackPosition = playbackPosition;
+
+        for (routeSegmentIndex = 0; routeSegmentIndex < durations.size(); ++routeSegmentIndex) {
+            if (routeSegmentPlaybackPosition - durations.get(routeSegmentIndex) <= 0) {
+                break;
+            }
+
+            routeSegmentPlaybackPosition -= durations.get(routeSegmentIndex);
+        }
+
+        Coordinate routeSegmentStart = coordinates.get(routeSegmentIndex);
+        // i+1 is safe because coordinates contains last segment's end coordinate as well
+        Coordinate routeSegmentEnd = coordinates.get(routeSegmentIndex+1);
+
+        long routeSegmentDuration = durations.get(routeSegmentIndex);
+        double routeSegmentPlaybackRatio = (double)routeSegmentPlaybackPosition / routeSegmentDuration;
+
+        return routeSegmentStart.getSplitPoint(routeSegmentEnd, routeSegmentPlaybackRatio);
     }
 }
