c31c2f627fb7e22dae286545d688d6db0c5b8b88
patelmohit <35036073+patelmohit@users.noreply.github.com>
2020-08-03 12:35:38 -0400

    Part 4: Handling when the fallback playlist should be triggered (#68)
    * Handled scenarios where fallback playlist will get triggered
    
    * Made TAG not the tag of a tag
    
    * Removed leftover import from merge conflict
    
    * Renamed isFallbackTriggered and added more documentation

--- app/src/main/java/com/cs446/group7/bruno/spotify/SpotifyPlayerService.java
+++ app/src/main/java/com/cs446/group7/bruno/spotify/SpotifyPlayerService.java
@@ -38,6 +38,12 @@ class SpotifyPlayerService implements MusicPlayer {
 
     private PlayerState currentPlayerState;
     private BrunoPlaylist playlist;
+
+    // Indicates if the player has been stopped and subsequently triggered fallback playlist behavior
+    private boolean isFallbackTriggered = false;
+    // Indicates if the player is done initializing and is playing the first song of the playlist
+    private boolean isPlayerStarted = false;
+    // Indicates if the player has received information about the first song of the playlist
     private boolean hasReachedFirstSong = false;
 
     public SpotifyPlayerService() {
@@ -119,6 +125,20 @@ class SpotifyPlayerService implements MusicPlayer {
                     if (playerState == null || playerState.equals(currentPlayerState)) return;
 
                     Log.d(TAG, "Received new PlayerState: " + playerState.toString());
+
+                    // Detect and handle fallback behaviour.
+                    // Since we only have one fallback playlist, we only trigger fallback once.
+                    // Note that the track could be null when the player has stopped.
+                    if (!isFallbackTriggered && didPlayerStopPlaying(playerState)) {
+                        Log.w(TAG, "Fallback playlist triggered.");
+                        isFallbackTriggered = true;
+                        for (MusicPlayerSubscriber subscriber : spotifyServiceSubscribers) {
+                            subscriber.onFallback();
+                        }
+                        // Early return since triggering onTrackChanged is not useful when switching playlists
+                        return;
+                    }
+
                     Track track = playerState.track;
 
                     if (track != null) {
@@ -155,6 +175,38 @@ class SpotifyPlayerService implements MusicPlayer {
         if (isConnected()) {
             SpotifyAppRemote.disconnect(mSpotifyAppRemote);
         }
+    }
+
+    // Checks if we need to switch to a fallback playlist by examining the current player state
+    private boolean didPlayerStopPlaying(PlayerState currentState) {
+        // Ignore player state changes until the player begins playing the first song of the playlist
+        if (!isPlayerStarted) {
+            // Need to make sure that the track is not null and is equal to the first song of the playlist
+            boolean isBeginningOfPlaylist = currentState.track != null
+                    && convertToBrunoTrack(currentState.track).equals(playlist.getTracks().get(0));
+            // Then check if the first song is currently playing and at a non-zero playback position
+            isPlayerStarted =
+                    isBeginningOfPlaylist && currentState.playbackSpeed != 0.0 && currentState.playbackPosition > 0;
+            if (isPlayerStarted) {
+                Log.d(TAG, "The player has started playing.");
+            }
+            return false;
+        }
+        // Since the player has started playing from this point, we don't have to worry about the track being null
+
+        // This case happens when Spotify appears to be playing a song (i.e. not paused), but the player is "stuck" and
+        // not playing music
+        boolean stoppedInSongMiddle = !currentState.isPaused && currentState.playbackSpeed == 0.0;
+
+        // This case happens when Spotify returns to the beginning of the song and is paused.
+        // Note: Spotify usually returns to the beginning of the playlist but it is possible to return to the
+        // beginning of the current song within the playlist, so this case assumes that it could be any song
+        boolean stoppedInSongBeginning =
+                currentState.isPaused && currentState.playbackSpeed == 0.0
+                        && currentState.playbackPosition == 0;
+        if (stoppedInSongBeginning) { Log.d(TAG, "The song has stopped in the beginning."); }
+        if (stoppedInSongMiddle) { Log.d(TAG, "The song has stopped in the middle."); }
+        return stoppedInSongBeginning || stoppedInSongMiddle;
     }
 
     // Plays the playlist which is set by setPlaylist()
