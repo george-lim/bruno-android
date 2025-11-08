32612e0d145fab1a7ffbf2524f81cf62ed752af8
George Lim <lim.george@me.com>
2025-11-08 17:35:00 +0000

    Update workflows

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.bruno.android"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
+        versionCode = 6
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
